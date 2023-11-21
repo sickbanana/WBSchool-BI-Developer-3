dfa7c841d9d6b6d6a84c76d8c8cab19cf26cc65b
sickbanana <serzh.karasev.1999@mail.ru>
2023-11-21 15:13:53 +0300

    task 2

--- hw_1.sql
+++ hw_1.sql
@@ -11,6 +11,29 @@ select src_office_id
     , uniq(rid_hash) qty
     , any(rid_hash) rid_example
 from history.assembly_task
-where date_diff('day', create_dt, now()) <= 4
+where date_diff('day', dt, now()) < 4
 group by src_office_id
 order by qty desc;
+
+-- 02
+-- По офису оформления src_office_id Электросталь вывести кол-во qty уникальных Сортированных заказов за каждый час dt_h,
+--   за последние 4е суток.
+-- Использовать справочник имен BranchOffice для вывода имен Офисов office_name.
+-- В блоке фильтрации использовать идентификатор офиса Электросталь (см.справочник Офисов).
+-- Использовать функцию toStartOfHour() для работы с датами для колонки dt_h.
+-- Для каждого офиса добавить колонку rid с любым номером заказа для примера.
+-- Добавить 2 колонки dt_min dt_max, которые показывают даты первого и последнего статуса в каждом часе.
+-- Упорядочить по колонке dt_h.
+-- Колонки: src_office_id, office_name, dt_h, qty, rid_example, dt_min, dt_max
+
+select src_office_id
+    , dictGet('dictionary.BranchOffice','office_name', toUInt64(src_office_id)) office_name
+    , toStartOfHour(dt) dt_h
+    , uniq(rid_hash) qty
+    , any(rid_hash) rid_example
+    , min(dt) dt_min
+    , max(dt) dt_max
+from history.sorted
+where src_office_id = 241542 and date_diff('day', create_dt, now()) < 4
+group by dt_h, src_office_id
+order by dt_h
