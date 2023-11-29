e12e0962f2704d36e49d22db49042e57b8e0d3b6
sickbanana <serzh.karasev.1999@mail.ru>
2023-11-29 22:33:09 +0300

    hw_4 task 1

---
+++ hw_4.sql
@@ -0,0 +1,54 @@
+-- ДЗ.
+-- Задание 1. Исследовать таблицу турникет history.turniket.
+
+-- Сколько записей в таблице.
+select count() qty_all
+from history.turniket
+
+-- Сколько записей на каждый день.
+select toDate(dt) dt_date
+    , count() qty_day
+from history.turniket
+group by dt_date
+
+-- Сколько записей на каждый день по каждому офису.
+select toDate(dt) dt_date, office_id
+    , dictGet('dictionary.BranchOffice','office_name', office_id) office_name
+    , count() qty_day
+from history.turniket
+group by  dt_date, office_id
+order by dt_date, office_id
+
+-- Сколько уникальных сотрудников в таблице.
+select uniq(employee_id) qty_employee
+from history.turniket
+
+-- Сколько уникальных сотрудников на каждый день.
+select toDate(dt) dt_date
+    , uniq(employee_id) qty_employee_day
+from history.turniket
+group by dt_date
+
+-- Сколько уникальных сотрудников на каждый день по каждому офису.
+select toDate(dt) dt_date, office_id
+    , dictGet('dictionary.BranchOffice','office_name', office_id) office_name
+    , uniq(employee_id) qty_employee_day
+from history.turniket
+group by  dt_date, office_id
+order by dt_date, office_id
+
+-- Кол-во входов.
+select countIf(is_in, is_in = 1) qty_in
+from history.turniket
+
+-- Кол-во выходов.
+select countIf(is_in, is_in = 0) qty_out
+from history.turniket
+
+-- Среднее кол-во входов на каждого сотрудника.
+select countIf(is_in, is_in = 1) / uniq(employee_id) avg_in
+from history.turniket
+
+-- Среднее кол-во выходов на каждого сотрудника.
+select countIf(is_in, is_in = 0) / uniq(employee_id) avg_out
+from history.turniket
