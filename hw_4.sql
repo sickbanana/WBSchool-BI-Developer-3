40cb0db311868461b14d2dd3f4be12b09237102c
sickbanana <serzh.karasev.1999@mail.ru>
2023-11-30 19:18:02 +0300

    hw_4 task 1 fix

--- hw_4.sql
+++ hw_4.sql
@@ -30,20 +30,18 @@ from history.turniket
 group by dt_date
 
 -- Сколько уникальных сотрудников на каждый день по каждому офису.
-select toDate(dt) dt_date, office_id
-    , dictGet('dictionary.BranchOffice','office_name', office_id) office_name
-    , uniq(employee_id) qty_employee_day
-from history.turniket
-group by  dt_date, office_id
-order by dt_date, office_id
-
 -- Кол-во входов.
 -- Кол-во выходов.
 -- Среднее кол-во входов на каждого сотрудника.
 -- Среднее кол-во выходов на каждого сотрудника.
-select countIf(is_in, is_in = 1) qty_in
+select toDate(dt) dt_date, office_id
+    , dictGet('dictionary.BranchOffice','office_name', office_id) office_name
+    , uniq(employee_id) qty_emp_day
+    , countIf(is_in, is_in = 1) qty_in
     , countIf(is_in, is_in = 0) qty_out
-    , qty_in / uniq(employee_id) avg_in
-    , qty_out / uniq(employee_id) avg_out
+    , round(qty_in / qty_emp_day) avg_in
+    , round(qty_out / qty_emp_day) avg_out
 from history.turniket
+group by  dt_date, office_id
+order by dt_date, office_id
 # это все одним запросом надо, а не 4
